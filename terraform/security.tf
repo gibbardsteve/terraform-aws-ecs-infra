b6ee99827299d59402a52150a646acdcc794d318
Steve Gibbard <steve.gibbard@ons.gov.uk>
2024-06-10 11:57:00 +0100

    Add Web application firewall

--- terraform/security.tf
+++ terraform/security.tf
@@ -28,33 +28,170 @@ resource "aws_security_group" "allow_rules_load_balancer" {
   }
 }
 
-# SG - Service specific - move to service module
-# consider restricting traffic here
-# resource "aws_security_group" "allow_rules_service" {
-#   name        = "allow-rule-service"
-#   description = "Allow inbound traffic on port ${var.container_port} from ${var.from_port} on the service"
-#   vpc_id      = module.vpc.vpc_id
+# Create a web application firewall
+# Define IP sets
+resource "aws_wafv2_ip_set" "allowed_ips_alb_response_subdomain" {
+  name        = "allowed-ips-alb-response-subdomain"
+  description = "Allowed IPs for the alb-response subdomain"
+  scope       = "REGIONAL"
 
-#   ingress {
-#     from_port   = var.from_port
-#     to_port     = var.container_port
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  # Add the IP addresses that are allowed to access the alb-response subdomain
+  # loopback is allowed in this example - Must UPDATE with your allowed IP lists
+  ip_address_version = "IPV4"
+  addresses          = ["127.0.0.1/32"]
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+}
 
-# To restrict to VPN and ONS onsite
-# cidr_blocks = ["194.34.204.36/32",
-#                "194.34.204.37/32",
-#                "194.34.206.36/32",
-#                "194.34.206.37/32",
-#                "155.190.13.19/32",
-#                "155.190.35.77/32",
-#                "155.190.60.38/32"]
+resource "aws_wafv2_ip_set" "allowed_ips_default_response_subdomain" {
+  name        = "allowed-ips-default-response-subdomain"
+  description = "Allowed IPs for the default-response subdomain"
+  scope       = "REGIONAL"
+
+  ip_address_version = "IPV4"
+
+  # Add the IP addresses that are allowed to access the default-response subdomain
+  # loopback is allowed in this example - Must UPDATE with your allowed IP lists
+  addresses = ["127.0.0.1/32"
+  ]
+
+}
+
+resource "aws_wafv2_rule_group" "ip_allow_rule_group" {
+  capacity = 20
+  name     = "ip-allow-rule-group"
+  scope    = "REGIONAL"
+
+  rule {
+    name     = "alb-response-rule"
+    priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          ip_set_reference_statement {
+            arn = aws_wafv2_ip_set.allowed_ips_alb_response_subdomain.arn
+          }
+        }
+
+        statement {
+          byte_match_statement {
+            search_string = "default-response.${local.url}"
+            field_to_match {
+              single_header {
+                name = "host"
+              }
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+            positional_constraint = "EXACTLY"
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "alb-response-metric"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "default-response-rule"
+    priority = 2
+
+    action {
+      allow {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          ip_set_reference_statement {
+            arn = aws_wafv2_ip_set.allowed_ips_default_response_subdomain.arn
+          }
+        }
+
+        statement {
+          byte_match_statement {
+            search_string = "default-response.${local.url}"
+            field_to_match {
+              single_header {
+                name = "host"
+              }
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+            positional_constraint = "EXACTLY"
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "default-response-metric"
+      sampled_requests_enabled   = false
+    }
+  }
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "ips-allowed-metric"
+    sampled_requests_enabled   = false
+  }
+}
+
+resource "aws_wafv2_web_acl" "web_firewall" {
+  name  = "waf-allowed-ips"
+  scope = "REGIONAL"
+
+  default_action {
+    block {}
+  }
+
+  rule {
+    name     = "allow-rule-1"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      rule_group_reference_statement {
+        arn = aws_wafv2_rule_group.ip_allow_rule_group.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  tags = {
+    Tag1 = "Value1"
+    Tag2 = "Value2"
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
+# Associate WAF rules with ALB
+resource "aws_wafv2_web_acl_association" "alb_association" {
+  resource_arn = aws_lb.service_lb.arn
+  web_acl_arn  = aws_wafv2_web_acl.web_firewall.arn
+}
